ad006a978275342558f0aaf389c7aabbf0dfca5a
A7640S <lt75425@gmail.com>
2024-05-22 17:32:41 +0000

    Deploy website - based on f433c61b226923c448b4093d0afab83b19b94e6f

--- redux/communautes/action.js
+++
@@ -1,8 +0,0 @@
-import constants from "./constantes";
-
-export function onGetCommunaute(challenges) {
-  const action = { type: constants.ON_GET_communauteS, challenges };
-  return (dispatch) => {
-    return dispatch(action);
-  };
-}
