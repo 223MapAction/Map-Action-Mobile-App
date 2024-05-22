f4a933952f8a7ff953fc371e4855e888e53c8d73
A7640S <lt75425@gmail.com>
2024-05-22 17:32:41 +0000

    Deploy website - based on f433c61b226923c448b4093d0afab83b19b94e6f

--- ios/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  MapActionMobile
-//
-//  Created by Kaicedra Consulting SAS on 23/2/2024.
-//
-
-import Foundation
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-    collectCoverage: true,
-    coverageReporters: ['text', 'cobertura'],
-};
