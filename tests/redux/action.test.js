28f5c6629329ce3cea3138f641518048ee25ba8b
A7640S <lt75425@gmail.com>
2024-06-05 18:03:38 +0000

    generating test

--- tests/redux/action.test.js
+++ tests/redux/action.test.js
@@ -7,46 +7,55 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('Redux Actions', () => {
-  it('should create an action to get incidents', () => {
-    const expectedAction = {
-      type: constants.ON_GET_INCIDENTS,
-      challenges: [/* Array of challenges */],
-    };
-    expect(actions.onGetIncidents([/* Array of challenges */])).toEqual(expectedAction);
-  });
-
-  it('should create an action to add an incident', () => {
-    const expectedAction = {
-      type: constants.ON_ADD_INCIDENT,
-      challenge: {/* Challenge object */},
-    };
-    expect(actions.onAddIncident({/* Challenge object */})).toEqual(expectedAction);
-  });
-
-  it('should create an action to delete an incident', () => {
-    const expectedAction = {
-      type: constants.ON_DELETE_INCIDENT,
-      challenge: {/* Challenge object */},
-    };
-    expect(actions.onDeleteIncident({/* Challenge object */})).toEqual(expectedAction);
-  });
-
-  it('should create an action to edit an incident', () => {
-    const expectedAction = {
-      type: constants.ON_EDIT_INCIDENT,
-      challenge: {/* Challenge object */},
-    };
-    expect(actions.onEditIncident({/* Challenge object */})).toEqual(expectedAction);
-  });
-
   it('should dispatch an action to get incidents', () => {
+    const challenges = []; // Exemple de tableau de challenges
+    const expectedActions = [
+      {
+        type: constants.ON_GET_INCIDENTS,
+        challenges,
+      },
+    ];
     const store = mockStore({});
-    const expectedActions = [{
-      type: constants.ON_GET_INCIDENTS,
-      challenges: [/* Array of challenges */],
-    }];
-    store.dispatch(actions.onGetIncidents([/* Array of challenges */]));
-    expect(store.getActions()).toEqual(expectedActions);
+    store.dispatch(actions.onGetIncidents(challenges)); // Appeler l'action asynchrone sans `.then()`
+    expect(store.getActions()).toEqual(expectedActions); // Vérifier l'action immédiatement
   });
 
+  it('should dispatch an action to add an incident', () => {
+    const challenge = {}; // Exemple d'objet challenge
+    const expectedActions = [
+      {
+        type: constants.ON_ADD_INCIDENT,
+        challenge,
+      },
+    ];
+    const store = mockStore({});
+    store.dispatch(actions.onAddIncident(challenge)); // Appeler l'action asynchrone sans `.then()`
+    expect(store.getActions()).toEqual(expectedActions); // Vérifier l'action immédiatement
+  });
+
+  it('should dispatch an action to delete an incident', () => {
+    const challenge = {}; // Exemple d'objet challenge
+    const expectedActions = [
+      {
+        type: constants.ON_DELETE_INCIDENT,
+        challenge,
+      },
+    ];
+    const store = mockStore({});
+    store.dispatch(actions.onDeleteIncident(challenge)); // Appeler l'action asynchrone sans `.then()`
+    expect(store.getActions()).toEqual(expectedActions); // Vérifier l'action immédiatement
+  });
+
+  it('should dispatch an action to edit an incident', () => {
+    const challenge = {}; // Exemple d'objet challenge
+    const expectedActions = [
+      {
+        type: constants.ON_EDIT_INCIDENT,
+        challenge,
+      },
+    ];
+    const store = mockStore({});
+    store.dispatch(actions.onEditIncident(challenge)); // Appeler l'action asynchrone sans `.then()`
+    expect(store.getActions()).toEqual(expectedActions); // Vérifier l'action immédiatement
+  });
 });
